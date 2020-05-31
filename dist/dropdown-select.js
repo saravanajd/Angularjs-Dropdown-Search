024be1b04c02918dd494c2c77862fd35eb37120e
saravana jd <saravana.jd@gmail.com>
2020-05-31 20:28:12 +0530

    Added item change event
    * Added item change event
    * Change the layout design
    * Change the dropdown style

--- dist/dropdown-select.js
+++ dist/dropdown-select.js
@@ -1,23 +1,26 @@
-(function(window, angular) {
+(function (window, angular) {
   'use strict';
 
   const apps = angular.module('DropDownSelect', []);
 
   apps.run([
     '$templateCache',
-    function($templateCache) {
+    function ($templateCache) {
       $templateCache.put(
         'template/dropdownSelect.html',
         [
           '<div class="ng-dropdown-container">',
           '    <div class="ng-dropdown-text">',
-          '        <span>{{labelText}}</span>',
+          '        <span>{{ddModel[ddLabel] ? ddModel[ddLabel] : "-- Select -- "}}</span>',
           '    </div>',
           '    <ul class="ng-dropdown-options">',
           '        <li class="ng-dropdown-item">',
-          '            <input type="text" ng-model="searchText" style="padding: 0.2rem 1rem;width: 100%">',
+          '            <input type="text" class="ng-dropdown-search" ng-model="searchText">',
           '        </li>',
-          '        <li class="ng-dropdown-item" ',
+          '        <li class="ng-dropdown-item" ng-click="Clear()" ng-class="{\'selected\':!ddModel[ddLabel]}">',
+          '            -- Select --',
+          '        </li>',
+          '        <li class="ng-dropdown-item" ng-class="{\'selected\':item === ddModel}" ',
           '        ng-repeat="item in ddData | filter:ddSearchFilter" data-item="{{item}}" ng-click="SelectItem($event,item)">',
           '            {{item[ddLabel]}}',
           '        </li>',
@@ -29,10 +32,9 @@
   ]);
 
   apps.directive('dropdownSelect', [
-    function() {
+    function () {
       return {
         restrict: 'E',
-        // require: "^ngModel",
         replace: true,
         scope: {
           ddModel: '=',
@@ -44,40 +46,49 @@
           '$scope',
           '$element',
           '$document',
-          function($scope, $element, $document) {
-            console.log('$element', $element, '$document', $document);
+          'filterFilter',
+          function ($scope, $element, $document, filterFilter) {
 
             $scope.searchText = '';
-            $scope.labelText = '-- select --';
-            if ($scope.ddModel && $scope.ddModel[$scope.ddLabel]) {
-              $scope.labelText = $scope.ddModel[$scope.ddLabel];
-            }
-
-            $scope.ddSearchFilter = function(item) {
+            $scope.ddSearchFilter = function (item) {
               return (
                 !$scope.searchText ||
                 (item[$scope.ddLabel] &&
                   item[$scope.ddLabel]
-                    .toLowerCase()
-                    .indexOf($scope.searchText.toLowerCase()) > -1)
+                  .toLowerCase()
+                  .indexOf($scope.searchText.toLowerCase()) > -1)
               );
             };
 
-            $scope.SelectItem = function($event, item) {
+            $scope.SelectItem = function ($event, item) {
               $scope.ddModel = item;
-
-              console.log(
-                '$scope.ddModel',
-                $scope.ddModel,
-                '$scope.ddData',
-                $scope.ddData
-              );
-
-              $scope.labelText = $scope.ddModel[$scope.ddLabel];
               $element.removeClass('show');
+              $scope.searchText = '';
+              setTimeout(function () {
+                $scope.$eval($scope.ddChange);
+              }, 100);
             };
 
-            $element.on('click', function($event) {
+            $scope.Clear = function () {
+              $scope.ddModel = '';
+              $element.removeClass('show');
+              $scope.searchText = '';
+              setTimeout(function () {
+                $scope.$eval($scope.ddChange);
+              }, 100);
+            }
+
+            $scope.$watch("ddData", function (newValue, oldValue) {
+              if (newValue != oldValue) {
+                const SelectItem = filterFilter(newValue, $scope.ddModel);
+                if (!SelectItem || SelectItem.length < 1) {
+                  $scope.ddModel = '';
+                }
+              }
+            });
+
+
+            const onDropDownSelectClick = function ($event) {
               const ele = angular.element($event.target);
 
               const isTarget = ele.closest('.ng-dropdown-text').length === 1;
@@ -85,9 +96,9 @@
               if (isTarget) {
                 $element.toggleClass('show');
               }
-            });
+            };
 
-            $document.on('mouseup', function($event) {
+            const onDocumentMouseUp = function ($event) {
               if (!$event || !$event.target) {
                 return;
               }
@@ -97,21 +108,22 @@
 
               if (!isDropdownContainer) {
                 $element.removeClass('show');
+                $scope.searchText = '';
               }
+            };
+
+            $element.on('click', onDropDownSelectClick);
+
+            $document.on('mouseup', onDocumentMouseUp);
+
+            $scope.$on('$destroy', function () {
+              element.off('click', onDropDownSelectClick);
+              $document.off('mouseup', onDocumentMouseUp);
             });
           }
         ],
-        // templateUrl: 'dropdownSelect.html' // 'template/dropdownSelect.html'
-        templateUrl: 'template/dropdownSelect.html'
+        templateUrl: 'template/dropdownSelect.html' //'dropdownSelect.html'
       };
     }
   ]);
-
-  function IsString(value) {
-    return typeof value === 'string';
-  }
-
-  function IsArray(value) {
-    return typeof value === 'object';
-  }
 })(window, window.angular);
